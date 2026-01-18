669c5b3e7450362d446cb2edb9a788ec35960d5d
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-01-19 06:18:21 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := device/amlogic/p281
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_FIRST_API_LEVEL := 29
-PRODUCT_CHARACTERISTICS := tv
 PRODUCT_BUILD_RECOVERY_IMAGE := true
 PRODUCT_BUILD_SYSTEM_IMAGE := false
 PRODUCT_BUILD_BOOT_IMAGE := false
@@ -15,13 +14,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-    
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-　　 update_engine \
-    update_verifier \
-    update_engine_sideload
 
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
