773d0afb8055731a741dfc2a837c1c4121c5855b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-01-18 19:54:50 +0900

    Update twrp_p281.mk

--- twrp_p281.mk
+++ twrp_p281.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
